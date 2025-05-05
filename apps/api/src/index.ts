2c67e287edcee24cbb758fce5fc105019c7fa8f3
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-05 19:08:32 +0200

    setup app with fastify

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'userid' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'userid' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
