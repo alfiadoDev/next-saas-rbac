e754b0e5f8e4329c17f10678b88ad2c43a498069
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-12 19:16:25 +0200

    list organizations

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,6 +1,8 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
+import { getOrganizations } from '@/http/get-organizations'
+
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import {
   DropdownMenu,
@@ -12,7 +14,9 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
-export function OrganizationSwitcher() {
+export async function OrganizationSwitcher() {
+  const { organizations } = await getOrganizations()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:ring-primary flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
@@ -27,13 +31,17 @@ export function OrganizationSwitcher() {
       >
         <DropdownMenuGroup>
           <DropdownMenuLabel>Organizations</DropdownMenuLabel>
-          <DropdownMenuItem>
-            <Avatar className="mr-2 size-5">
-              <AvatarImage src="https://github.com/alfiadoDev.png" />
-              <AvatarFallback />
-            </Avatar>
-            <span className="line-clamp-1">Xtreme it</span>
-          </DropdownMenuItem>
+          {organizations.map((org) => (
+            <DropdownMenuItem key={org.id} asChild>
+              <Link href={`/org/${org.slug}`}>
+                <Avatar className="mr-2 size-5">
+                  {org.avatarUrl && <AvatarImage src={org.avatarUrl} />}
+                  <AvatarFallback />
+                </Avatar>
+                <span className="line-clamp-1">{org.name}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
