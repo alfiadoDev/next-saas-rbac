76df933647b5fa296d69135d714f18ea6cf3cf7f
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-12 22:08:18 +0200

    using enviroment variables

--- apps/web/src/app/auth/actions.tsx
+++ apps/web/src/app/auth/actions.tsx
@@ -1,14 +1,15 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
   const githubUrl = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubUrl.searchParams.set('client_id', 'Ov23liYdBnXHe1agNvdh')
+  githubUrl.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubUrl.searchParams.set(
     'redirect_url',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   githubUrl.searchParams.set('scope', 'user')
 
