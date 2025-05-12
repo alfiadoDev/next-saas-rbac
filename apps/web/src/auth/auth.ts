b3429ec371f64d447be624ce92c08b6c3e8ab975
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-12 17:02:22 +0200

    getting authenticated user

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,28 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookiesStory = await cookies()
 
   return !!cookiesStory.get('token')?.value
+}
+
+export async function auth() {
+  const cookiesStory = await cookies()
+
+  const token = cookiesStory.get('token')?.value
+
+  if (!token) redirect('/auth/sign-in')
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+    // eslint-disable-next-line prettier/prettier
+  } catch (error) {
+  }
+
+  redirect('/api/auth/sign-out')
 }
