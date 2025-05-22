b295ed83b9d1f2a4b0af9d060db8c410e88b1fee
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-22 22:12:26 +0200

    accept and reject invite

--- apps/web/src/components/pending-invites/index.tsx
+++ apps/web/src/components/pending-invites/index.tsx
@@ -1,15 +1,43 @@
+'use client'
+
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { Check, UserPlus2, X } from 'lucide-react'
+import { useState } from 'react'
+
+import { getPendingInvites } from '@/http/get-pending-invites'
 
 import { Button } from '../ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { acceptInviteAction, rejectInviteAction } from './actions'
 
 dayjs.extend(relativeTime)
 
 export function PendingInvites() {
+  const [isOpen, setIsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  const { data } = useQuery({
+    queryKey: ['pending-invites'],
+    queryFn: getPendingInvites,
+    enabled: isOpen,
+  })
+
+  async function handleAcceptInvite(inviteId: string) {
+    await acceptInviteAction(inviteId)
+
+    queryClient.invalidateQueries({ queryKey: ['pending-invites'] })
+  }
+
+  async function handleRejectInvite(inviteId: string) {
+    await rejectInviteAction(inviteId)
+
+    queryClient.invalidateQueries({ queryKey: ['pending-invites'] })
+  }
+
   return (
-    <Popover>
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
         <Button size="icon" variant="ghost">
           <UserPlus2 className="size-4" />
@@ -17,29 +45,48 @@ export function PendingInvites() {
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-80 space-y-2">
-        <span className="text-sm font-medium">Pending invites (2)</span>
+        <span className="text-sm font-medium">
+          Pending invites ({data?.pendingInvites.length ?? 0})
+        </span>
 
-        <div className="space-y-2">
-          <p className="text-muted-foreground text-sm leading-relaxed">
-            <span className="text-foreground font-medium">John Doe</span>{' '}
-            invited you to join{' '}
-            <span className="text-foreground font-medium">
-              Acme Inc (Admin)
-            </span>{' '}
-            <span className="">{dayjs(new Date()).fromNow()}</span>
-          </p>
+        {data?.pendingInvites.length === 0 && (
+          <p className="text-muted-foreground text-sm">no invites found</p>
+        )}
 
-          <div className="flex gap-2">
-            <Button size="xs" variant="outline">
-              <Check className="mr-1.5 size-3" />
-              Accept
-            </Button>
-            <Button size="xs" variant="ghost" className="text-muted-foreground">
-              <X className="mr-1.5 size-3" />
-              Reject
-            </Button>
+        {data?.pendingInvites.map((invite) => (
+          <div className="space-y-2" key={invite.id}>
+            <p className="text-muted-foreground text-sm leading-relaxed">
+              <span className="text-foreground font-medium">
+                {invite.author?.name}
+              </span>{' '}
+              invited you to join{' '}
+              <span className="text-foreground font-medium">
+                {invite.organization.name}
+              </span>{' '}
+              <span className="">{dayjs(invite.createdAt).fromNow()}</span>
+            </p>
+
+            <div className="flex gap-2">
+              <Button
+                size="xs"
+                variant="outline"
+                onClick={() => handleAcceptInvite(invite.id)}
+              >
+                <Check className="mr-1.5 size-3" />
+                Accept
+              </Button>
+              <Button
+                size="xs"
+                variant="ghost"
+                className="text-muted-foreground"
+                onClick={() => handleRejectInvite(invite.id)}
+              >
+                <X className="mr-1.5 size-3" />
+                Reject
+              </Button>
+            </div>
           </div>
-        </div>
+        ))}
       </PopoverContent>
     </Popover>
   )
