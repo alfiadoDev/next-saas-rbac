300a93e8a5e8f78e69fa0e796d028da581b8fbb6
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-05-22 13:38:40 +0200

    invite new users

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,9 +1,12 @@
 'use server'
 
-import type { Role } from '@saas/auth'
+import { type Role, rolesSchema } from '@saas/auth'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
 
 import { getCurrentOrg } from '@/auth/auth'
+import { createInvite } from '@/http/create-invite'
 import { removeMember } from '@/http/remove-member'
 import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
@@ -40,4 +43,49 @@ export async function revokeInviteAction(inviteId: string) {
   })
 
   revalidateTag(`${currentOrg}/invites`)
+}
+
+const inviteSchema = z.object({
+  email: z.string().email(),
+  role: rolesSchema,
+})
+
+export async function createInviteAction(data: FormData) {
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, role } = result.data
+  const org = await getCurrentOrg()
+
+  try {
+    await createInvite({
+      org: org!,
+      role,
+      email,
+    })
+
+    revalidateTag(`${org}/invites`)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the invite',
+    errors: null,
+  }
 }
